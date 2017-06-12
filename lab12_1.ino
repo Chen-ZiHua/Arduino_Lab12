214f2655d0ab03321e03c2b3c8ae4c0096122204
陳姿華 <isa19970825@gmail.com>
2017-06-12 19:56:55 +0800

    Lab12_1使用 SoftwareSerial 範例程式, 建立與 ESP-8266 之連線

---
+++ lab12_1.ino
@@ -0,0 +1,33 @@
+#include <SoftwareSerial.h>
+#include "ESP8266.h"
+ESP8266 wifi(Serial);
+
+SoftwareSerial mySerial(11, 10); // RX, TX(arduino的)
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial) 
+  {
+    ; 
+  }
+
+
+  Serial.println("");
+  mySerial.begin(115200);
+ 
+  mySerial.println("AT\n");//　直接先送出一個AT出去
+}
+
+void loop()
+{ 
+  if (mySerial.available())
+  {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available())
+  {
+    mySerial.write(Serial.read());
+  }
+}
+
